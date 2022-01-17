5d80e54bc9e88122209ceae547ed1c709663008b
AleksandrDus <aleksandr.dus@privatbank.ua>
2022-01-17 22:45:44 +0200

    №12

---
+++ №12.js
@@ -0,0 +1,11 @@
+
+var arr = [1,2,3,-5,-2,1,-4]
+var sum = 0
+
+for (i = 0; i < arr.length; i ++) 
+    if (arr[i] > 0) {
+        
+        sum = sum + arr[i]
+       
+    }
+        console.log(sum)
