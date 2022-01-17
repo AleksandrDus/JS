97d92f9e8d64ba2115d61a683ee3eeb207b5be97
AleksandrDus <aleksandr.dus@privatbank.ua>
2022-01-17 22:16:59 +0200

    №3

---
+++ №3.js
@@ -0,0 +1,15 @@
+
+val = 'a'
+if (val === 'a') {
+    console.log('a')
+} else  if ((val  === 'b') || (val === 'c') || (val === 'd') || (val === 'e')) {
+
+    console.log('others')
+}
+
+else 
+
+    console.log('unknown')
+
+
+
