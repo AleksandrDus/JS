9a6c91a21f35c8afa055b8987fc44a97274ee23d
AleksandrDus <aleksandr.dus@privatbank.ua>
2022-01-21 23:33:05 +0200

    №38

---
+++ №38.js
@@ -0,0 +1,20 @@
+function f(arr) {
+   
+      console.log(arr.splice(0,1))
+   
+    if (arr.length == 0) {
+
+        return
+  
+    
+}      
+
+    
+    f(arr)
+          
+}  
+
+f([1,2,3]);
+
+    
+
