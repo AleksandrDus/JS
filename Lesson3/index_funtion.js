96682e4e1aaa6c797144ffbb86d18ad301451857
AleksandrDus <aleksandr.dus@privatbank.ua>
2022-01-22 00:03:45 +0200

    index_funk

---
+++ Lesson3/index_funtion.js
@@ -0,0 +1,23 @@
+
+
+
+function f(arr) {
+   
+      console.log(arr.splice(0,1))
+   
+    if (arr.length == 0) {
+
+        return
+  
+    
+}      
+
+    
+    f(arr)
+          
+}  
+
+f([1,2,3]);
+
+    
+
