ccd4982dad1de398f33c610e94170b4ccf587851
AleksandrDus <97900353+AleksandrDus@users.noreply.github.com>
2022-01-24 12:28:31 +0200

    Update №38.js

--- №38.js
+++ №38.js
@@ -1,6 +1,14 @@
 function f(arr) {
+   if (Array.isArray(arr) != true) {
+        throw new Error ('parameter type should be an array')
+    }
+    if (arr.length === 0) {
+        throw new Error ('parameter can`t be an empty')
+
+    }
+      
    
-      console.log(arr.splice(0,1))
+   console.log(arr.splice(0,1))
    
     if (arr.length == 0) {
 
